1552a1d59a93aa153df4c7b5e05713e4632e5e5a
HazelArevalo <arevalo.hja@gmail.com>
2026-01-26 00:05:51 -0800

    update-carousel-images

--- src/components/ControlledCarousel.jsx
+++ src/components/ControlledCarousel.jsx
@@ -16,7 +16,7 @@ function ControlledCarousel() {
           className="d-block w-100"
           src="https://images.pexels.com/photos/15460829/pexels-photo-15460829.jpeg?_gl=1*pqpom0*_ga*MTkxNDUwNDU2MS4xNzYwODQyOTMy*_ga_8JE65Q40S6*czE3Njk0MTIyOTYkbzE3JGcxJHQxNzY5NDEzNDkwJGo1OCRsMCRoMA.."
           alt="Colosseum, Rome"
-          style={{ maxHeight: '600px', objectFit: 'cover' }}
+          style={{ maxHeight: '700px', objectFit: 'cover' }}
         />
         <Carousel.Caption>
           <h3>Day 1: Explore Colosseum and Roman Forum 🏛️</h3>
@@ -28,7 +28,7 @@ function ControlledCarousel() {
           className="d-block w-100"
           src="https://images.pexels.com/photos/4863938/pexels-photo-4863938.jpeg?_gl=1*toexff*_ga*MTkxNDUwNDU2MS4xNzYwODQyOTMy*_ga_8JE65Q40S6*czE3Njk0MTIyOTYkbzE3JGcxJHQxNzY5NDEzMTUxJGoyNyRsMCRoMA.."
           alt="Gondola Ride, Venice"
-          style={{ maxHeight: '600px', objectFit: 'cover' }}
+          style={{ maxHeight: '700px', objectFit: 'cover' }}
         />
         <Carousel.Caption>
           <h3>Day 2: Gondola ride in Venice 🚤</h3>
@@ -40,7 +40,7 @@ function ControlledCarousel() {
           className="d-block w-100"
           src="https://images.pexels.com/photos/20821732/pexels-photo-20821732.jpeg?_gl=1*za6pab*_ga*MTkxNDUwNDU2MS4xNzYwODQyOTMy*_ga_8JE65Q40S6*czE3Njk0MTIyOTYkbzE3JGcxJHQxNzY5NDEyODgzJGo4JGwwJGgw"
           alt="Wine Tasting, Tuscany"
-          style={{ maxHeight: '600px', objectFit: 'cover' }}
+          style={{ maxHeight: '700px', objectFit: 'cover' }}
         />
         <Carousel.Caption>
           <h3>Day 3: Wine tasting in Tuscany 🍷</h3>
